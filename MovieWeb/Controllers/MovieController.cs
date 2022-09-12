819fdf91097eb9d0e1ffa672d494a3e04b22c8a2
Boghian Gabriel <boghiangabriel96@yahoo.com>
2022-09-12 11:54:22 +0300

    [Add] Another project that calls project API and displays information which is there

---
+++ MovieWeb/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieWeb.Models;
+
+namespace MovieWeb.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult GetAllMovies()
+        {
+            IEnumerable<Movie>? movies = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7165/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("Movies");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    //Add NuGetPackages -> Microsoft.AspNet.WebApi.Client  for shorter method
+                    var readTask = result.Content.ReadAsAsync<IList<Movie>>();
+
+                    readTask.Wait();
+
+                    movies = readTask.Result;
+                }
+                else //web api sent error response
+                {
+                    // log response status here....
+                    movies = Enumerable.Empty<Movie>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator");
+                }
+            }
+
+            return View(movies);
+        }
+    }
+}
---
+++ MovieWeb/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieWeb.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public DateTime RealeseDate { get; set; }
+    }
+}
