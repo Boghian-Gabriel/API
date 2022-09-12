a14ca17d6a8143d203d692a9675be538f8e3a497
Boghian Gabriel <boghiangabriel96@yahoo.com>
2022-09-12 13:35:03 +0300

    [Add] Create new movie

--- MovieWeb/Controllers/MovieController.cs
+++ MovieWeb/Controllers/MovieController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
 using MovieWeb.Models;
 
 namespace MovieWeb.Controllers
 {
     public class MovieController : Controller
     {
+        #region GetMethod
         public IActionResult GetAllMovies()
         {
             IEnumerable<Movie>? movies = null;
@@ -37,5 +39,51 @@ namespace MovieWeb.Controllers
 
             return View(movies);
         }
+        #endregion
+
+        #region "Post Method"
+        public IActionResult CreateMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateMovie(Movie movie)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7165/api/Movies");
+
+                //Http Post
+                var postTask = client.PostAsJsonAsync<Movie>("Movies", movie);
+                postTask.Wait();    
+
+                var result = postTask.Result;
+                //condition if it's ok
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("GetAllMovies");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server error. Please contact administrator");
+
+            return View(movie);
+        }
+        #endregion
+
+
+        #region "Put Method"
+        public IActionResult Put()
+        {
+            return View();
+        }
+        #endregion
+
+        #region "Delete Method"
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        #endregion
     }
 }
